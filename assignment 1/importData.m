fb04004607745e06e0594d6fa13550103f585b58
Tjitte <tp.dejong@mail.com>
2014-09-09 15:39:00 +0200

    Updated importData.m to import the data
    TrainingSamplesOfFeatures and TrainingTargetsOfSamples for training

---
+++ assignment 1/importData.m
@@ -0,0 +1,8 @@
+% import features for training
+TrainingSamplesOfFeatures = csvread(strcat(pwd,'\Data\features.txt'));
+
+% import targets for training
+TrainingTargetsOfSamples = csvread(strcat(pwd,'\Data\targets.txt'));
+
+% import features to calculate targets
+SamplesOfFeatures = csvread(strcat(pwd,'\Data\unknown.txt'));
