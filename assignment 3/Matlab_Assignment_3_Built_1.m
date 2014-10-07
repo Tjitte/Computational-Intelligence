55f0bfb9eb96b02b41e19e530bb6bc9b8cfa607c
borismul <boris.mulder@live.nl>
2014-10-07 16:59:50 +0200

    kan doolhoven inlezen

--- assignment 3/Matlab_Assignment_3_Built_1.m
+++ assignment 3/Matlab_Assignment_3_Built_1.m
@@ -1,6 +1,11 @@
 %% Variables
 
-
+clear all
+close all 
+clc
 
 %% Input Maze
 
+easyMaze = dlmread('Mazes\easy maze.txt');
+mediumMaze = dlmread('Mazes\medium maze.txt');
+hardMaze = dlmread('Mazes\hard maze.txt');
