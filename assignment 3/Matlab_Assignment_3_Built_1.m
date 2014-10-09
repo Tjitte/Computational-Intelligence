df88a7f6a031404145f932a97f4810d5b7a950f4
borismul <boris.mulder@live.nl>
2014-10-09 15:07:01 +0200

    vanalles

--- assignment 3/Matlab_Assignment_3_Built_1.m
+++ assignment 3/Matlab_Assignment_3_Built_1.m
@@ -6,6 +6,13 @@ clc
 
 %% Input Maze
 
-easyMaze = dlmread('Mazes\easy maze.txt');
-mediumMaze = dlmread('Mazes\medium maze.txt');
-hardMaze = dlmread('Mazes\hard maze.txt');
+% input of the mazes 
+easyMaze = dlmread('Mazes\easy maze.txt','',1,0);
+mediumMaze = dlmread('Mazes\medium maze.txt','',1,0);
+hardMaze = dlmread('Mazes\hard maze.txt','',1,0);
+
+% input of the starting and ending coordinates of the maze
+easyMaze_c = load('Mazes\easy coordinates.txt',',');
+mediumMaze_c = load('Mazes\medium coordinates.txt',',');
+hardMaze_c = load('Mazes\hard coordinates.txt',',');
+
