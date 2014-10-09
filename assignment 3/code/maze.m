cbeea888579f307c1ab8d2775fd8f04ca35c8003
mspanoghe <m.spanoghe@student.tudelft.nl>
2014-10-09 14:58:55 +0200

    variables

--- assignment 3/code/maze.m
+++ assignment 3/code/maze.m
@@ -4,4 +4,12 @@ iterations = 1;
 ants = 1;
 pheromone = 100;
 evaporation = 0.1;
-stopcriterian = 0.90;
+stopCriterion = 0.90;
+
+beginPoint = [1,1];
+endPoint = [16,16];
+
+pheromoneMap = zeros(
+
+position = beginPoint; 
+visited =  
