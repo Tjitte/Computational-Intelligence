266688fc9dfd8546099c7f509aca8a77823329d0
borismul <boris.mulder@live.nl>
2014-10-10 13:47:07 +0200

    ant loopt maar kan vastlopen als die z'n eigen pad tegenkomt

--- assignment 3/Matlab_Assignment_3_Built_1.m
+++ assignment 3/Matlab_Assignment_3_Built_1.m
@@ -3,9 +3,6 @@ clear all
 close all 
 clc
 
-
-
-
 %% Input Maze
 
 % input of the mazes 
@@ -41,7 +38,7 @@ mazeSize = size(training1Maze);
 
 pherMatrix=training1Maze;
 
-%%
+%% For now: 1 ant walking randomly through maze
 
 Pos = beginPos;
 q=1;
@@ -108,25 +105,33 @@ while Pos(1,1) ~= endPos(1,1) || Pos(1,2) ~= endPos(1,2)
         
     end
     
+    % creating a matrix for the pheromones in all posible directions
     pher = zeros(1,4);
-
-    for i=1:length(choicesProperty)
-        counter = 0;
+    
+    % loop through the directions
+    for i=1:length(choicesProperty)        
         
-
+        % if a direction is a posibility 
         if choicesProperty(i) == 1;
+            
+            % calculate the coordinates of the possible point
             NewPos=Pos + windDir(i,:);
-            disp(NewPos)
+            
+            % determine how much pheremones are on this spot
             pher(:,i) = pherMatrix(NewPos(1,1),NewPos(1,2));
-            disp(pher)
+            
         end
     end
     
-
+    % creating the probability's of choosing the directions
     probs = pher/sum(pher);
     
+    % creating a random number
     random = rand();
-    disp(probs)
+    
+    % if the random number lies between 0 and the probability of first direcition
+    % choose that one elseif the random number lies between the probability
+    % of the first and second direction choose that one etc
     
     if random<probs(1)
         Pos=Pos+windDir(1,:);
@@ -138,12 +143,51 @@ while Pos(1,1) ~= endPos(1,1) || Pos(1,2) ~= endPos(1,2)
         Pos=Pos+windDir(4,:);
     end
 
-    
+   % keep track of the route the ant took
    PosList(q,:) = Pos;
-   %TIJDELIJK !!!! 
-   %Pos=endPos;
     
 end
 
-plot(PosList(:,2),-PosList(:,1)+1)
-axis([0 mazeSize(1,2) -mazeSize(1,1) 0])
+%% plotting the route of an ant and plotting the maze with begin position as well as end position
+
+% plot the route of the ant
+plot(PosList(:,2),-PosList(:,1)+1,'r')
+
+% setting the axes ranges
+axis([-1 mazeSize(1,2)+2 -mazeSize(1,1)-1 2])
+hold on
+
+% looping through the whole maze
+for i=0:mazeSize(1,1)+1;
+    for j=0:mazeSize(1,2)+1;
+        
+        % if the i or j is not in the matrix create an X
+        if i < 1 || i > mazeSize(1,1) || j < 1 || j > mazeSize(1,2)
+            
+            plot(j,-i+1,'x','linewidth',20)
+
+        
+        else
+            
+            % if the position on the maze has a wall (0) plot an X there
+            if training1Maze(i,j)==0
+                
+                plot(j,-i+1,'x','linewidth',20)
+                
+            % if  the postion on the maze is the end position plot a green
+            % square
+            elseif i== endPos(1,1) && j== endPos(1,2)
+                
+                plot(j,-i+1, 'gs','linewidth',5)
+            
+            % if the position on the maze is the begin position plot a
+            % magenta square
+            elseif i== beginPos(1,1) && j== beginPos(1,2)
+                
+                plot(j,-i+1, 'ms','linewidth',5)
+                
+            end
+            
+        end
+    end
+end
